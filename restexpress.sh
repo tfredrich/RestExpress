b9a2d87461e29ac1704399b29d70c1098843d6fe
Todd Fredrich <tfredrich@gmail.com>
2011-02-14 16:40:45 -0700

    Enhanced app creation script and .project file to do text substitution on new application creation.

--- restexpress.sh
+++ restexpress.sh
@@ -14,18 +14,19 @@ cp $RESTEXPRESS_HOME/.classpath $1
 cp $RESTEXPRESS_HOME/.project $1
 cd $1
 
-# Update build.xml
-sed -i .orig s/KickStart/$1/g build.xml
+# Update build.xml & Eclipse project
+sed -i .orig s/[Kk]ick[Ss]tart/$1/g build.xml
+sed -i .orig s/[Kk]ick[Ss]tart/$1/g .project
 
 # Rename the 'kickstart' directories
-mv src/java/com/kickstart/ src/java/com/$1/
+mv src/java/com/kickstart/ src/java/com/$NAME_LWR/
 # packages
-find src/java/com/$1 -name '*.java' | xargs grep -l kickstart | xargs sed -i .orig s/kickstart/$NAME_LWR/g
+find src/java/com/$NAME_LWR -name '*.java' | xargs grep -l kickstart | xargs sed -i .orig s/kickstart/$NAME_LWR/g
 # class names
-find src/java/com/$1 -name '*.java' | xargs grep -li kickstart | xargs sed -i .orig s/[Kk]ick[Ss]tart/$1/g
+find src/java/com/$NAME_LWR -name '*.java' | xargs grep -li kickstart | xargs sed -i .orig s/[Kk]ick[Ss]tart/$1/g
 # file names
-mv src/java/com/$1/KickstartEnvironment.java src/java/com/$1/$1Environment.java
-mv src/java/com/$1/service/KickStartController.java src/java/com/$1/service/$1Controller.java
+mv src/java/com/$NAME_LWR/KickstartEnvironment.java src/java/com/$NAME_LWR/$1Environment.java
+mv src/java/com/$NAME_LWR/service/KickStartController.java src/java/com/$NAME_LWR/service/$1Controller.java
 
 #if {-z 'test/java/com/kickstart/'}
 #    mv test/java/com/kickstart/ test/java/com/$1/
