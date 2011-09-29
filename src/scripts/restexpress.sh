ca5fb656d178091c3715588177f07203b926f4a3
Todd Fredrich <tfredrich@gmail.com>
2011-09-29 12:19:28 -0600

    Moved restexpress.sh to src/scripts/ directory.

--- src/scripts/restexpress.sh
+++ src/scripts/restexpress.sh
@@ -34,8 +34,7 @@ find src/java/com/$NAME_LWR -name '*.java' | xargs grep -l kickstart | xargs sed
 # class names
 find src/java/com/$NAME_LWR -name '*.java' | xargs grep -li kickstart | xargs sed -i.orig s/[Kk]ick[Ss]tart/$1/g
 # file names
-mv src/java/com/$NAME_LWR/KickstartEnvironment.java src/java/com/$NAME_LWR/$1Environment.java
-mv src/java/com/$NAME_LWR/service/KickStartController.java src/java/com/$NAME_LWR/service/$1Controller.java
+mv src/java/com/$NAME_LWR/controller/KickStartController.java src/java/com/$NAME_LWR/controller/$1Controller.java
 
 #if {-z 'test/java/com/kickstart/'}
 #    mv test/java/com/kickstart/ test/java/com/$1/
